e3cad963ec2452bbb4403bf1443c30e482bd8024
S1rker <carter.a.moon@gmail.com>
2025-10-29 15:05:57 -0500

    add some debug features

--- src/Custom_Vector/main.cpp
+++ src/Custom_Vector/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+int* array;
+int size=0;
+int capacity=5;
+
+void PushBack(int value)
+{
+    array[size] = value;
+    ++size;
+}
+
 int main()
 {
+    capacity = 5;
+
+    array = new int[capacity];
+    PushBack(3);
+    PushBack(5);
+
+    delete [] array;
+
     return 0;
 }
